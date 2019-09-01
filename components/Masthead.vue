12af968ad5a6d043d552be0c066699a2d350feb6
Jeremy <me@jeremypoole.ca>
2019-08-31 19:40:00 -0700

    cleanup styles

--- components/Masthead.vue
+++ components/Masthead.vue
@@ -18,34 +18,29 @@ section {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  // margin-top: 10px;
-  .applicator {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 10px;
-    margin-bottom: 10px;
-    // box-shadow: 0 2px 5px rgba(0,0,0,.26);
-    // border: 3px solid black;
-    border-radius: 100%;
-    // padding: 10px 30px;
-    img {
+  margin-top: 10px;
+  // .applicator {
+  //   display: flex;
+  //   align-items: center;
+  //   justify-content: center;
+  //   margin-top: 10px;
+  //   margin-bottom: 10px;
+  //   border-radius: 100%;
+  //   img {
+  //     width: 60%;
+  //     margin-bottom: 10px;
+  //     margin-top: 20px;
+  //   }
 
-      // max-width: 60%;
-      width: 60%;
-      margin-bottom: 10px;
-      margin-top: 20px;
-    }
+  // }
 
-  }
-
-  .overlay {
-    // position: absolute;
-    h1,h2 {
-      // position: relative;
-      // top: 36vw;
-    }
-  }
+  // .overlay {
+  //   // position: absolute;
+  //   h1,h2 {
+  //     // position: relative;
+  //     // top: 36vw;
+  //   }
+  // }
   h1 {
     margin-bottom: 40px;
     text-transform: uppercase;
@@ -54,9 +49,6 @@ section {
     font-size: 1.3rem;
     color: #222;
     color: rgb(32,32,32);
-    // color: #ffe480;
-    // color: rgb(255,228,128);
-    // color: rgb(144,130,80);
     letter-spacing: 1px;
     .smaller {
       font-size: 1.1rem;
@@ -69,20 +61,7 @@ section {
     -webkit-text-stroke: 1.0px #00000020;
     font-weight: 800;
     font-size: 2.2em;
-    color: #111111e0;
-    color: rgb(144,130,80);
-    color: rgb(36,33,20);
-    color: #ffc800;
-    color: #ffffff00;
-    color: #4971CC;
-    color: #ead989;
     color: #cca449;
-    // color: #cc6449;
-    // background: linear-gradient(90deg, rgba(255,200,0,1) 10%, rgba(255,200,0,0.4878151089537377) 75%);
-    // background: rgb(199,154,60);
-    // background: linear-gradient(90deg, rgba(199,154,60,1) 16%, rgba(234,220,143,1) 79%);
-    // background-clip: text;
-    // -webkit-text-fill-color: transparent;
     word-spacing: 5px;
     padding-bottom: 15px;
   }
