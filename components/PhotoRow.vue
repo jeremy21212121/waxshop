15770974bf86af1612ada5c6c51a42f8bee029e1
Jeremy <me@jeremypoole.ca>
2019-10-04 23:03:35 -0700

    add scss mixins, border radius, box shadow

--- components/PhotoRow.vue
+++ components/PhotoRow.vue
@@ -7,37 +7,27 @@
 </template>
 
 <style lang="scss" scoped>
+@import '~/scss/mixins/spaceEvenlyHack.scss';
+@import '~/scss/mixins/boxShadows.scss';
+
 div {
   margin-top: 20px;
   margin-bottom: 20px;
   margin: 10px auto;
   max-width: 100%;
   display: flex;
-  justify-content: space-between;
-  // justify-content: space-evenly;
-  &:before,
-  &:after {
-    // ungodly hack to work around edge/safari's terrible flexbox implementation. Specifically, justify-content: space-evenly.
-    // see https://stackoverflow.com/questions/47534216/how-to-make-css-justify-contentspace-evenly-fallback-to-space-between-on-safari
-    content: "";
-    display: block;
-  }
-  justify-content: space-between;
+  // justify-content: space-evenly iOS support via mixin
+  @include space-evenly-hack;
   img {
     width: 30%;
     height: 100%;
-    // height: auto;
+    @include bs-white-1;
   }
 }
 @media screen and (min-width: 740px) {
   div {
     height: 100%;
-    justify-content: space-between;
-    &:before,
-    &:after {
-      // ungodly hack to work around other ungodly hack to work around edge/safari's terrible flexbox implementation
-      display: none;
-    }
+    @include undo-space-evenly-hack;
     img {
       width: 32%;
     }
