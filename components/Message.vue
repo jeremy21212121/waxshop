74cd9ff2363f9e89e1bc7d47770b52f5f47226b3
Jeremy <me@jeremypoole.ca>
2019-10-24 00:27:32 -0700

    add srcset to background image with multiple resolutions

--- components/Message.vue
+++ components/Message.vue
@@ -1,7 +1,13 @@
 <template>
   <section class="message">
     <div class="img">
-      <img src="~/assets/bw_landing2.jpg" alt="A woman with smooth legs" aria-hidden="true">
+      <img
+        :srcset="imageArrayToSrcset(image.srcset)"
+        :src="image.srcset[0].src"
+        :sizes="image.sizes"
+        :alt="image.alt"
+        aria-hidden="true"
+      >
       <div class="msg-wrapper">
         <contact-form />
       </div>
@@ -15,6 +21,37 @@ import ContactForm from '~/components/ContactForm.vue'
 export default {
   components: {
     ContactForm
+  },
+  data () {
+    return {
+      image: {
+        srcset: [
+          {
+            src: require('~/assets/legs_bw-740.jpg'),
+            width: 740
+          },
+          {
+            src: require('~/assets/legs_bw-1110.jpg'),
+            width: 1110
+          },
+          {
+            src: require('~/assets/legs_bw-1480.jpg'),
+            width: 1480
+          },
+          {
+            src: require('~/assets/legs_bw-1608.jpg'),
+            width: 1608
+          }
+        ],
+        sizes: '(min-width: 740px) 740px, 100vw',
+        alt: 'A woman with smooth legs'
+      }
+    }
+  },
+  methods: {
+    imageArrayToSrcset (imageArray) {
+      return imageArray.map(img => `${img.src} ${img.width}w`).join(', ')
+    }
   }
 }
 </script>
