eb6cfaa1621842bd59d49fbd4e10f8b19806c8d4
Jeremy <me@jeremypoole.ca>
2019-12-06 23:36:27 -0800

    delete commented-out code

--- components/PhotoRow.vue
+++ components/PhotoRow.vue
@@ -1,9 +1,4 @@
 <template>
-  <!-- <div>
-    <img src="~/assets/lobby_sm.jpg" alt="the lobby of the wax shop">
-    <img src="~/assets/prod_crop_sm.jpg" alt="waxing products">
-    <img src="~/assets/entry_sm.jpg" alt="the entry of the wax shop">
-  </div> -->
   <div>
     <client-only>
       <light-gallery
@@ -12,7 +7,6 @@
         :disable-scroll="true"
         @close="index = null"
       />
-      <!-- </light-gallery> -->
     </client-only>
     <ul>
       <li
@@ -78,35 +72,6 @@ export default {
   },
   data () {
     return {
-      // images: [
-      //   {
-      //     title: 'Our lovely lobby',
-      //     url: require('assets/entryway-740.jpg'),
-      //     srcset: require('assets/entryway-740.jpg') + ' 1x, ' + require('assets/entryway-1480.jpg') + ' 2x',
-      //     alt: 'entryway',
-      //     thumb: {
-      //       srcset: require('assets/entryway_thumb-238.jpg') + ' 1x, ' + require('assets/entryway_thumb-476.jpg') + ' 2x'
-      //     }
-      //   },
-      //   {
-      //     title: 'Premium products',
-      //     url: require('assets/products-740.jpg'),
-      //     srcset: require('assets/products-740.jpg') + ' 1x, ' + require('assets/products-1480.jpg') + ' 2x',
-      //     alt: 'premium products',
-      //     thumb: {
-      //       srcset: require('assets/products_thumb-238.jpg') + ' 1x, ' + require('assets/products_thumb-476.jpg') + ' 2x'
-      //     }
-      //   },
-      //   {
-      //     title: 'Tidy treatment rooms',
-      //     url: require('assets/room-740.jpg'),
-      //     srcset: require('assets/room-740.jpg') + ' 1x, ' + require('assets/room-1480.jpg') + ' 2x',
-      //     alt: 'tidy treatment room',
-      //     thumb: {
-      //       srcset: require('assets/room_thumb-238.jpg') + ' 1x, ' + require('assets/room_thumb-476.jpg') + ' 2x'
-      //     }
-      //   }
-      // ],
       index: null
     }
   }
@@ -139,33 +104,4 @@ div {
   }
 }
 
-// div {
-//   margin-top: 20px;
-//   margin-bottom: 20px;
-//   margin: 10px auto;
-//   max-width: 100%;
-//   display: flex;
-//   // justify-content: space-evenly old iOS safari support via mixin
-//   @include space-evenly-hack;
-//   img {
-    // width: 30%;
-    // height: 100%;
-    // @include bs-white-1;
-    // // border-radius: 2px;
-    // @include border-radius-standard;
-//   }
-// }
-// @media screen and (min-width: $break-l) {
-//   div {
-//     // height: 100%;
-//     // @include undo-space-evenly-hack;
-//     ul {
-//       li {
-//         img {
-//           width: 32%;
-//         }
-//       }
-//     }
-//   }
-// }
 </style>
