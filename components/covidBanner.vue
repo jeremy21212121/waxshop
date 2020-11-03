684cd617e964a866a618a6ebff3aad3a8afe9b0e
Jeremy <me@jeremypoole.ca>
2020-11-03 15:30:26 -0800

    *Actually* revert temporary emergency changes. Fixing botched attempt at the same in commit 8c948e5f9913e9ae7d50eaf5d02d2755cbfb487e

--- components/covidBanner.vue
+++ components/covidBanner.vue
@@ -1,9 +1,8 @@
 <template>
-  <nuxt-link :to="'/contact'" title="Contact us" class="covid">
+  <nuxt-link :to="'/covid-19'" title="Covid-19 Safety Measures" class="covid">
     <img src="~/assets/info.png" alt="Info" aria-hidden="true">
     <span class="banner">
-      <!-- Now open Monday - Saturday with extensive safety measures in place -->
-      Our phones are temporarily down. {{ isMobile ? 'Tap' : 'Click' }} here to contact us.
+      Now open Monday - Saturday with extensive safety measures in place
     </span>
     <read-more />
   </nuxt-link>
@@ -15,18 +14,18 @@ export default {
   name: 'CovidBanner',
   components: {
     ReadMore
-  },
-  computed: {
-    isMobile() {
-      return this.$el && this.$el.clientWidth && this.$el.clientWidth < 740
-    }
   }
+  // computed: {
+  //   isMobile() {
+  //     return this.$el && this.$el.clientWidth && this.$el.clientWidth < 740
+  //   }
+  // }
 }
 </script>
 <style lang="scss" scoped>
 .covid {
   background-color: #355992;
-  background-color: #990099;
+  // background-color: #990099;
   width: 100%;
   padding: 4px;
   display: flex;
